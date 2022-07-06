078d3436d8f784b09fc977271d21022ee4cf9b02
Pratap-Agile <pratapagile009@gmail.com>
2022-07-06 15:01:02 +0530

    second commit

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const data = [
-  { name: "Pratap", email: "pratap@gmail.com", contact: "123456789" },
-  { name: "Bhavesh", email: "bhavesh@gmail.com", contact: "789456123" },
+  { name: "Herry", email: "harry@gmail.com", contact: "123456789" },
+  { name: "Thor", email: "thor@gmail.com", contact: "789456123" },
   { name: "Jams", email: "jams@gmail.com", contact: "456789123" },
 ];
 module.exports = data;
